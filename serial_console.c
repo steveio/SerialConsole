d4e58e69f73c8c5bc8cba8f9a0c1e0c8cad7f6dd
steveio.com <info@steveio.com>
2020-03-08 23:44:38 +0000

    Simple Linux Serial Console Parser for Arduino CLI

--- serial_console.c
+++ serial_console.c
@@ -26,30 +26,6 @@
 #include <unistd.h> // write(), read(), close()
 
 
-#define  B300	0000007
-#define  B600	0000010
-#define  B1200	0000011
-#define  B1800	0000012
-#define  B2400	0000013
-#define  B4800	0000014
-#define  B9600	0000015
-#define  B19200	0000016
-#define  B38400	0000017
-#define  B57600   0010001
-#define  B115200  0010002
-#define  B230400  0010003
-#define  B460800  0010004
-#define  B500000  0010005
-#define  B576000  0010006
-#define  B921600  0010007
-#define  B1000000 0010010
-#define  B1152000 0010011
-#define  B1500000 0010012
-#define  B2000000 0010013
-#define  B2500000 0010014
-#define  B3000000 0010015
-#define  B3500000 0010016
-#define  B4000000 0010017
 #define BUFFER_SIZE 8
 
 // Define the function to be called when ctrl-c (SIGINT) signal is sent to process
